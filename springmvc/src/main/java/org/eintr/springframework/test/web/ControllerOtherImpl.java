41765c9ea5981fc65e8640b477dfe108e1dd4984
impact-eintr <yixingwei4@gmail.com>
2024-01-10 23:01:11 +0800

    测试了多个Controller的支持，接下来看一下注解版的实现 通过指定PostHandler GetHandler 这些 这个估计可能要用到MethodMapping

---
+++ springmvc/src/main/java/org/eintr/springframework/test/web/ControllerOtherImpl.java
@@ -0,0 +1,23 @@
+package org.eintr.springframework.test.web;
+
+import cn.hutool.http.HttpStatus;
+import org.eintr.springframework.web.servlet.ModelAndView;
+import org.eintr.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStreamWriter;
+
+public class ControllerOtherImpl implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setStatus(HttpStatus.HTTP_OK);
+        ServletOutputStream out = response.getOutputStream();
+        OutputStreamWriter ow = new OutputStreamWriter(out,"UTF8");
+        ow.write("这是测试 ControllerOtherImpl");
+        ow.flush();
+        ow.close();
+        return null;
+    }
+}
